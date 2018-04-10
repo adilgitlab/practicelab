836eb76487d71700637c9a21afadaa39ed4d1043
adilgitlab <adilalikhan4u@gmail.com>
2018-04-10 11:21:07 +0530

    comitting the alameen maven project

---
+++ alameen/src/main/java/com/alameen/App.java
@@ -0,0 +1,13 @@
+package com.alameen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
